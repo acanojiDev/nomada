71578747896053a07e8768dac0b6fb1f83680824
jotaeme890 <permanentjavi@gmail.com>
2025-12-26 17:58:03 +0100

    Created auth page and route

--- nomada/src/app/app.routes.ts
+++ nomada/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthPage } from './features/auth-page/auth-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthPage,
+  }
+];
---
+++ nomada/src/app/features/auth-page/auth-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-page.html',
+  styleUrl: './auth-page.scss',
+})
+export class AuthPage {
+
+}
