d651a90090dd9cd806b953eff311d587ae12eab8
jotaeme890 <permanentjavi@gmail.com>
2025-12-28 13:32:58 +0100

    Added login and register components, auth service and environment variables

---
+++ nomada/src/app/core/services/auth.ts
@@ -0,0 +1,33 @@
+import { Injectable, signal } from '@angular/core';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private supabase: SupabaseClient;
+  currentUser = signal<User | null>(null);
+
+  constructor() {
+    this.supabase = createClient(
+      environment.SUPABASE_URL,
+      environment.SUPABASE_KEY
+    );
+  }
+
+  async signIn(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+    if (data.user) {
+      this.currentUser.set(data.user);
+    }
+    return { data, error };
+  }
+
+  async signUp(email: string, password: string) {
+    return await this.supabase.auth.signUp({ email, password });
+  }
+}
